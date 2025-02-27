d20e50c097c029689da5b2d7b399aeed65bf187e
unknown <juan.brodriguez@labinfo.is.escuelaing.edu.co>
2025-02-27 13:39:02 -0500

    Creacio de proyecto maven

---
+++ demo/demo/src/test/java/parcialCorteCVDS/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package parcialCorteCVDS.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
